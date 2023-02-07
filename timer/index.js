2e92fe89d017a674e2c4c57aa2eaeb9a3b3aa35d
Arya <2243728@cognizant.com>
2023-02-07 12:09:49 +0530

    timer

---
+++ timer/index.js
@@ -0,0 +1,25 @@
+const durationInput = document.querySelector("#duration");
+const startButton = document.querySelector("#start");
+const pauseButton = document.querySelector("#pause");
+const circle = document.querySelector("circle");
+
+const perimeter = circle.getAttribute("r") * Math.PI * 2;
+circle.setAttribute("stroke-dasharray", perimeter);
+
+let duration;
+
+const timer = new Timer(durationInput, startButton, pauseButton, {
+  onStart(totalDuration) {
+    console.log("Timer started");
+    duration = totalDuration;
+  },
+  onTick(timeRemaining) {
+    circle.setAttribute(
+      "stroke-dashoffset",
+      (perimeter * timeRemaining) / duration - perimeter
+    );
+  },
+  onComplete() {
+    console.log("Timer is completed");
+  },
+});
